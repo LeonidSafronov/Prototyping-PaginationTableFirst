215700ffcb3bf3569c6a0a0b9f4116639a8f4bda
LeonidSafronov <safronovleonid@gmail.com>
2022-05-11 12:04:05 +0300

    Initial Commit

---
+++ PaginationTableFirst/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PaginationTableFirst
+//
+//  Created by Leonid Safronov on 11.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
